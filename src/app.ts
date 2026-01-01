d07c4da986b5137ec37ec44823f879b7441defcb
jh-salman <jhsalman53@gmail.com>
2026-01-01 15:48:48 +0600

    ContactX-server folder structure setup

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+
+import express, { Request, Response } from "express";
+
+
+
+export const app = express()
+app.use(express.json())
+
+app.get("/", (req:Request, res: Response)=>{
+    res.send("Hello World")
+})
